4da3348e8495ebf637139f534e5bd9d0d760bf1b
root <root@ip-172-30-0-149.(none)>
2015-04-05 18:59:04 +0000

    added new hosts rewrite

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# we need to make a fully conical version of the hostname for the agent
+HOSTNAME="$(hostname)"
+IP_ADDRESS="$(hostnamei -i)"
+echo "${IP_ADDRESS} ${HOSTNAME}.localdomain.net ${HOSTNAME}">/root/hosts
+cat /root/hosts_base>>/root/hosts
+cp /root/hosts /etc/hosts
+
+# start services
 /etc/init.d/ntpd start
 /etc/init.d/cloudera-scm-server-db start
 /etc/init.d/cloudera-scm-agent start
